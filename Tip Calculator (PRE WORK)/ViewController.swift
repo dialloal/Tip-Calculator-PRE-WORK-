4af9e2075cfd2d05d9208bc6933dd2c6ea13aaf3
Alhassan Diallo <dialloal@msu.edu>
2021-08-28 15:25:23 -0400

    Initial Commit

---
+++ Tip Calculator (PRE WORK)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator (PRE WORK)
+//
+//  Created by Alhassan Diallo on 8/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tip Calculator (PRE WORK)Tests/Tip_Calculator__PRE_WORK_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Tip_Calculator__PRE_WORK_Tests.swift
+//  Tip Calculator (PRE WORK)Tests
+//
+//  Created by Alhassan Diallo on 8/28/21.
+//
+
+import XCTest
+@testable import Tip_Calculator__PRE_WORK_
+
+class Tip_Calculator__PRE_WORK_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
